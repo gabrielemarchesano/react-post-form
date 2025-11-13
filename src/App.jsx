2a8f11148e376b27f558a83cd306d69eb3e2ec5b
gabrielemarchesano <marchesano.gabriele@gmail.com>
2025-11-13 12:41:47 +0100

    create the structure of the form

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,15 @@ function App() {
 
   return (
     <>
-      
+      <div className="container">
+        <h1 className="text-center pt-5">C'è POST per te!</h1>
+        <form>
+          <input type="text" placeholder="Inserisci nome e cognome o il tuo nome d'arte"/>
+          <input type="text" placeholder="Inserisci il titolo del post"/>
+          <textarea name="" id="" placeholder="Inserisci il testo del post"></textarea>
+          <button>Pubblica</button>
+        </form>
+      </div>
     </>
   )
 }
