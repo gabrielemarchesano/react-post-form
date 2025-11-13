33b2cf2853564fcb921d09a5c6c311101cc45432
gabrielemarchesano <marchesano.gabriele@gmail.com>
2025-11-13 12:54:19 +0100

    complete the form structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,37 @@
 function App() {
 
 
+
   return (
     <>
       <div className="container">
-        <h1 className="text-center pt-5">C'è POST per te!</h1>
-        <form>
-          <input type="text" placeholder="Inserisci nome e cognome o il tuo nome d'arte"/>
-          <input type="text" placeholder="Inserisci il titolo del post"/>
-          <textarea name="" id="" placeholder="Inserisci il testo del post"></textarea>
-          <button>Pubblica</button>
-        </form>
+        <header>
+          <h1 className="text-center pt-5">C'è POST per te!</h1>
+        </header>
+        <main className="py-4">
+          <form>
+            <div>
+              <label htmlFor="author">Autore</label>
+              <input name="author" type="text" placeholder="Inserisci nome e cognome o il tuo nome d'arte"/>
+            </div>
+            <div>
+              <label htmlFor="title">Titolo</label>
+              <input name="title" type="text" placeholder="Inserisci il titolo del post"/>
+            </div>
+            <div>
+              <label htmlFor="text">Testo</label>
+              <textarea name="text" id="text" placeholder="Inserisci il testo del post"></textarea>
+            </div>
+            <div>
+              <input type="radio" name="public" id="" />
+              <label htmlFor="public">Pubblico</label>
+              <input type="radio" name="draft" id="" />
+              <label htmlFor="draft">Bozza</label>
+              
+            </div>
+            <button onSubmit={null}>Invia</button>
+          </form>
+        </main>
       </div>
     </>
   )
